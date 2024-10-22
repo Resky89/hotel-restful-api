3aeba54b75f4efa0df612b6c87383b2ae261ba20
Muhammad Resky Prabowo Sutejo <155411644+Resky89@users.noreply.github.com>
2024-10-22 18:21:53 +0700

    Update GuestsRoute.js

--- src/routes/GuestsRoute.js
+++ src/routes/GuestsRoute.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const guestController = require('../controllers/GuestsController');
 
-router.get('/guests', guestController.getAllGuests);
-router.get('/guests/:id', guestController.getGuestById);
-router.post('/guests', guestController.createGuest);
-router.put('/guests/:id', guestController.updateGuest);
-router.delete('/guests/:id', guestController.deleteGuest);
-
-module.exports = router;
