30e485d5f51f5b7784dee4cfde616be2faf4cc2b
Muhammad Resky Prabowo Sutejo <155411644+Resky89@users.noreply.github.com>
2024-10-22 18:22:30 +0700

    Update RoomsRoute.js

--- src/routes/RoomsRoute.js
+++ src/routes/RoomsRoute.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const roomsController = require('../controllers/RoomsController');
 
-router.get('/rooms', roomsController.getAllRooms);
-router.get('/rooms/:id', roomsController.getRoomById);
-router.post('/rooms', roomsController.createRoom);
-router.put('/rooms/:id', roomsController.updateRoom);
-router.delete('/rooms/:id', roomsController.deleteRoom);
-
-module.exports = router;
