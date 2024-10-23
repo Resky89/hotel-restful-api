00ed4670092f2ddc1e3bb66eeffdf67fd9dbe109
Fauzan Taslim <107458554+dayatt16@users.noreply.github.com>
2024-10-23 13:20:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-// Memuat dotenv hanya jika NODE_ENV bukan 'production'
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const routes = require("./src/routes/ApiRoutes");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-// Menggunakan variabel dari .env
-const express = require('express');
-const app = express();
-
-// Menggunakan PORT dari .env
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+app.use("/api", routes);
 
-// Menjalankan server
 app.listen(PORT, () => {
-    console.log(`Server berjalan di port ${PORT}`);
+  console.log(`Server berjalan di port http://localhost:${PORT}`);
 });
