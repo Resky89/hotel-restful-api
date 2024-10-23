905f5ac4b67087733b05ae1b4594ae58dd858649
Muhammad Resky Prabowo Sutejo <155411644+Resky89@users.noreply.github.com>
2024-10-23 13:56:06 +0700

    RES-21 Added GuestsService.js

--- src/services/GuestsService.js
+++ src/services/GuestsService.js
@@ -1,1 +1,26 @@
 
+const Guest = require('../models/GuestsModel');
+
+const guestService = {
+  async getAllGuests() {
+    return await Guest.findAll();
+  },
+
+  async getGuestById(id) {
+    return await Guest.findById(id);
+  },
+
+  async createGuest(guestData) {
+    return await Guest.create(guestData);
+  },
+
+  async updateGuest(id, guestData) {
+    return await Guest.update(id, guestData);
+  },
+
+  async deleteGuest(id) {
+    return await Guest.delete(id);
+  },
+};
+
+module.exports = guestService;
