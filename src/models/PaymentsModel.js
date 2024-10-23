0a3066a7de3199395a9a21cb8b08e12d56138269
Ari eka prianda <156278683+ariekakagerou@users.noreply.github.com>
2024-10-23 11:54:32 +0700

    RES-10 Added PaymentsModel.js

--- src/models/PaymentsModel.js
+++ src/models/PaymentsModel.js
@@ -1,1 +1,51 @@
+// src/models/payment.js
+const db = require("../config/database");
+
+// Model untuk pembayaran
+const Payment = {
+  create: async (paymentData) => {
+    const query =
+      "INSERT INTO payment (booking_id, amount, payment_method, payment_status, transaction_id, payment_date) VALUES (?, ?, ?, ?, ?, ?)";
+    const [result] = await db.query(query, [
+      paymentData.booking_id,
+      paymentData.amount,
+      paymentData.payment_method,
+      paymentData.payment_status,
+      paymentData.transaction_id,
+      paymentData.payment_date,
+    ]);
+    return result;
+  },
+  getAll: async () => {
+    const query = "SELECT * FROM payment";
+    const [results] = await db.query(query);
+    return results;
+  },
+  getById: async (id) => {
+    const query = "SELECT * FROM payment WHERE id = ?";
+    const [result] = await db.query(query, [id]);
+    return result;
+  },
+  update: async (id, paymentData) => {
+    const query =
+      "UPDATE payment SET booking_id = ?, amount = ?, payment_method = ?, payment_status = ?, transaction_id = ?, payment_date = ? WHERE id = ?";
+    const [result] = await db.query(query, [
+      paymentData.booking_id,
+      paymentData.amount,
+      paymentData.payment_method,
+      paymentData.payment_status,
+      paymentData.transaction_id,
+      paymentData.payment_date,
+      id,
+    ]);
+    return result;
+  },
+  delete: async (id) => {
+    const query = "DELETE FROM payment WHERE id = ?";
+    const [result] = await db.query(query, [id]);
+    return result;
+  },
+};
+
+module.exports = Payment;
 
