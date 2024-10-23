691bac2e7a43b3b9af840f5e65c5a09dd700facb
Fauzan Taslim <107458554+dayatt16@users.noreply.github.com>
2024-10-23 13:19:04 +0700

    RES-18 Added BookingsValidation.js

--- src/validations/BookingsValidation.js
+++ src/validations/BookingsValidation.js
@@ -1,1 +1,16 @@
+const Joi = require('joi');
+
+const bookingSchema = Joi.object({
+  guest_id: Joi.number().integer().required(),
+  room_id: Joi.number().integer().required(),
+  check_in_date: Joi.date().iso().required(),
+  check_out_date: Joi.date().iso().min(Joi.ref("check_in_date")).required(),
+  total_guests: Joi.number().integer().min(1).required(),
+  status: Joi.string().valid("pending", "confirmed", "cancelled").required(),
+  total_price: Joi.number().precision(2).positive().required(),
+});
+
+module.exports = {
+  bookingSchema
+};
 
